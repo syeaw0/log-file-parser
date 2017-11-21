72c7492c4d2c47694ec52b72ef1d3deb44f36cce
syeaw0 <samyeaw@yahoo.com>
2017-11-21 13:46:48 -0800

    Updated Parser.h file

--- Documents/Parser.h
+++ Documents/Parser.h
@@ -1,6 +1,26 @@
+/*********************************************************
+* CLASS Parser
+* --------------------------------------------------------
+* This class is the central class for this program. It's
+* main jobs are to read a log file, initialize a data 
+* object based on that file, and then create an output
+* with the data provided by the data object in the
+* specified format
+**********************************************************/
+#include <string>
 using namespace std;
 
 class Parser
 {
+	/*ATTRIBUTES*/
+	private:
+	
+	/*METHODS*/
+	public:
+	// Default Constructor
+	Parser();
 
-}
+	// GENERAL METHODS
+	void readFile(string filename);
+	void readFile(/*File Object*/);
+};
