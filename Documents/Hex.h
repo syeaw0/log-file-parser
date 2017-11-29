0a4f447b181ffa946c58fdf98a40246ab20c9861
HectorC25 <hectorc@csu.fullerton.edu>
2017-11-28 19:02:41 -0800

    Update to Hex.h and add Hex.cpp

--- Documents/Hex.h
+++ Documents/Hex.h
@@ -16,9 +16,9 @@ class Hex{
 		Hex();
 
 		//this method will convert the hex to binary
-		void toBinary()
+		toBinary()
 		//take a hex and convert to binary and store the binary
 		
 		//This method will retrieve the stored binary number and return it
-		void getBinary(){}
+		getBinary(){}
 };
