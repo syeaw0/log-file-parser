3ad0ff82159a9d4978bd757ca01b606894fb3040
HectorC25 <hectorc@csu.fullerton.edu>
2017-11-30 00:51:08 -0800

    Update to Hex.h

--- src/Hex.h
+++ src/Hex.h
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 //takes a string and stores, accept a string, convert to binary
 /***************************************************************
 Class Hex
-
-This class will take a hexidecimal and convert it to a binary 
+This class will take a hexidecimal and convert it to a binary
 number which will then be stored to be used in other class files.
 ***************************************************************/
 using namespace std;
 
-class Hex{
+class Hex {
 
-	public:
-		//default constructor
-		Hex();
-
-		//this method will convert the hex to binary
-		void toBinary(){
-			//take a hex and convert to binary and store the binary
-		}
-		
-		//This method will retrieve the stored binary number and return it
-		void getBinary(){}
+public:
+	void HexToBin(string s);
+private:
+	vector<char> binum;
+	vector<char> word;
+	vector<int>::const_iterator i;
 };
